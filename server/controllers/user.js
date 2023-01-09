d04ef99d91e9490e50b967dfe465e817b2c9febc
Pavel Zolotin <zolotinpavel@gmail.com>
2023-01-09 17:07:09 +0500

    add user verification

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,5 +1,45 @@
-const test = (req, res) => {
-    res.json('Its successful')
+import {createError} from "../error.js";
+import User from '../models/User.js'
+
+export const updateUser = async(req, res, next) => {
+    if (req.params.id === req.user.id) {
+        try {
+            const updatedUser = await User.findOneAndUpdate(req.params.id, {
+                    $set: res.body
+                },
+                {
+                    new: true
+                }
+            )
+            res.status(200).json(updatedUser)
+        } catch (err) {
+            next(err)
+        }
+    } else {
+        return next(createError(403, 'You can update only your account'))
+    }
 }
 
-export default test
+export const deleteUser = (req, res, next) => {
+
+}
+
+export const getUser = (req, res, next) => {
+
+}
+
+export const subscribe = (req, res, next) => {
+
+}
+
+export const unsubscribe = (req, res, next) => {
+
+}
+
+export const like = (req, res, next) => {
+
+}
+
+export const dislike = (req, res, next) => {
+
+}
