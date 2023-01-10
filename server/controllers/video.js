55d59c5dde473be35cd0e0fae60d7a3bda33f356
Pavel Zolotin <zolotinpavel@gmail.com>
2023-01-10 14:27:20 +0500

    add search by query & by tags

--- server/controllers/video.js
+++ server/controllers/video.js
@@ -40,7 +40,7 @@ export const deleteVideo = async(req, res, next) => {
         const video = await Video.findById(req.params.id)
         if (!video) return next(createError(404, 'The video not found'))
         if (req.user.id === video.userId) {
-            const updatedVideo = await Video.findByIdAndDelete(
+            await Video.findByIdAndDelete(
                 req.params.id
             )
             res.status(200).json('The video has been deleted')
@@ -63,7 +63,7 @@ export const getVideo = async(req, res, next) => {
 
 export const addView = async(req, res, next) => {
     try {
-        const video = await Video.findByIdAndUpdate(req.params.id, {
+        await Video.findByIdAndUpdate(req.params.id, {
             $inc: {
                 views: 1
             }
@@ -98,11 +98,35 @@ export const subscribed = async(req, res, next) => {
         const subscribedChannels = user.subscribedUsers
 
         const list = await Promise.all(
-            subscribedChannels.map(channelId => {
-                return Video.find({userId: channelId})
+            subscribedChannels.map(async(channelId) => {
+                return await Video.find({userId: channelId})
             })
         )
-        res.status(200).json(list.flat().sort((a,b) => b.createdAt - a.createdAt))
+        res.status(200).json(list.flat().sort((a, b) => b.createdAt - a.createdAt))
+    } catch (err) {
+        next(err)
+    }
+}
+
+export const getByTag = async(req, res, next) => {
+    const tags = req.query.tags.split(',')
+    try {
+        const videos = await Video.find({tags: {$in: tags}}).limit(20)
+        res.status(200).json(videos)
+    } catch (err) {
+        next(err)
+    }
+}
+
+export const search = async(req, res, next) => {
+    const query = req.query.q
+    try {
+        const videos = await Video.find({
+            title: {
+                $regex: query, $options: 'i'
+            },
+        }).limit(40)
+        res.status(200).json(videos)
     } catch (err) {
         next(err)
     }
--- server/routes/videos.js
+++ server/routes/videos.js
@@ -1,6 +1,16 @@
 import express from 'express'
 
-import {addVideo, addView, trend, random, subscribed} from '../controllers/video.js'
+import {
+    addVideo,
+    getVideo,
+    deleteVideo,
+    addView,
+    trend,
+    random,
+    subscribed,
+    getByTag,
+    search
+} from '../controllers/video.js'
 import {verifyToken} from '../verifyToken.js'
 
 const router = express.Router()
@@ -8,11 +18,13 @@ const router = express.Router()
 //create a video
 router.post('/', verifyToken, addVideo)
 router.put('/:id', verifyToken, addVideo)
-router.delete('/:id', verifyToken, addVideo)
-router.get('/find/:id', addVideo)
+router.delete('/:id', verifyToken, deleteVideo)
+router.get('/find/:id', getVideo)
 router.put('/view/:id', addView)
 router.get('/trend', trend)
 router.get('/random', random)
 router.get('/subscribe', verifyToken, subscribed)
+router.get('/tags', getByTag)
+router.get('/search', search)
 
 export default router
